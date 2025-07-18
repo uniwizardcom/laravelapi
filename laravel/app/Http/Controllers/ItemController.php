e82d29dfd6f35338f83eb082d3f7b9973c98e3eb
Wojciech Niewiadomski gmail <niewiadomski.wojciech@gmail.com>
2025-07-18 11:40:28 +0200

    Redis cache

--- laravel/app/Http/Controllers/ItemController.php
+++ laravel/app/Http/Controllers/ItemController.php
@@ -4,15 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class ItemController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function index($id) {
+
+        $cachedBlog = Redis::get('items:' . $id);
+
+
+        if(isset($cachedBlog)) {
+            $blog = json_decode($cachedBlog, FALSE);
+
+            return response()->json([
+                'status_code' => 201,
+                'message' => 'Fetched from redis',
+                'data' => $blog,
+            ]);
+        } else {
+            $blog = Item::find($id);
+            Redis::set('items:' . $id, $blog);
+
+            return response()->json([
+                'status_code' => 201,
+                'message' => 'Fetched from database',
+                'data' => $blog,
+            ]);
+        }
     }
 
     /**
